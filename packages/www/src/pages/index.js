d071d970052f4663ab0a9ec56d8ddbb09afe8c87
Adeel Malik <adeelmalik1988@gmail.com>
2021-01-06 00:41:43 +0500

    added user netlify identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Container, Heading, Flex } from 'theme-ui'
+import netlifyIdentity from "netlify-identity-widget"
 
-export default props => (
-    <Container>
-        <Flex sx={{
-            flexDirection:"column",
-            padding: 3
+export default props => {
+    useEffect(() => {
+        netlifyIdentity.init({});
+
+    }, [])
+
+    return (
+        <Container>
+            <Flex sx={{
+                flexDirection: "column",
+                padding: 3
             }} >
-            <Heading as='h1' >Todo App</Heading>
-            <Button sx={{marginTop: 2, color: "black" }}  
-            onClick={()=>{alert("Login")}}
-            >Login</Button>
-        </Flex>
-    </Container>
-)
+                <Heading as='h1' >Todo App</Heading>
+                <Button sx={{ marginTop: 2, color: "black" }}
+                    onClick={() => { netlifyIdentity.open() }}
+                >Login</Button>
+            </Flex>
+        </Container>
+    )
+}
